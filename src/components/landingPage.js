8101a0709d133c90edf12ff5cc3fed625ec8df99
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-02-12 00:28:33 +0530

    adding mobile response in langing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -12,7 +12,7 @@ const LandingPage = () => {
         <div>
             { homePage ? (
                 <div className="flex items-center justify-center h-screen   bg-gradient-to-r from-pink-500 to-yellow-500">
-                    <div className="w-[full] h-[50%] max-w-md p-8 space-y-3 rounded-xl  bg-gradient-to-r from-amber-200 to-yellow-500 flex flex-col justify-around">
+                    <div className="md:w-[full] w-[80%] h-[50%] max-w-md p-8 space-y-3 rounded-xl  bg-gradient-to-r from-amber-200 to-yellow-500 flex flex-col justify-around">
                         <h1 className="text-center p-4 text-4xl text-white"> Wellcome to FORUM Application</h1>
                         <div className="flex justify-around">
                             <button onClick={e => { e.preventDefault(); setHosePage(false); setToSingup(false)}} className="block  bg-blue-500  hover:bg-blue-600 p-3  text-white  focus:outline-none rounded-lg ">Login</button> 
